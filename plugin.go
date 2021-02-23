5c395a2e88d8333b27fcaa3246fd016d3508e591
Gerard de Leeuw <gdeleeuw@leeuwit.nl>
2021-02-23 20:26:55 +0100

    Implement

--- plugin.go
+++ plugin.go
@@ -13,26 +13,36 @@ import (
 )
 
 // Config the plugin configuration.
-type Config struct{}
+type Config struct {
+	DefaultMode string            `json:"defaultmode"`
+	Headers     map[string]string `json:"headers,omitempty"`
+}
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
-	return &Config{}
+	return &Config{
+		DefaultMode: "keep",
+		Headers:     make(map[string]string),
+	}
 }
 
 // JwtLogging a JwtLogging plugin.
 type JwtLogging struct {
-	next     http.Handler
-	name     string
-	template *template.Template
+	next        http.Handler
+	defaultMode string
+	headers     map[string]string
+	name        string
+	template    *template.Template
 }
 
 // New created a new JwtLogging plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	return &JwtLogging{
-		next:     next,
-		name:     name,
-		template: template.New("jwt-logging").Delims("[[", "]]"),
+		defaultMode: config.DefaultMode,
+		headers:     config.Headers,
+		next:        next,
+		name:        name,
+		template:    template.New("jwt-logging").Delims("[[", "]]"),
 	}, nil
 }
 
@@ -48,8 +58,8 @@ func (a *JwtLogging) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 				return
 			}
 
-			jsonBody := string(decodedBody)
-			req.Header.Set("X-JWT", jsonBody)
+			// jsonBody := string(decodedBody)
+			// req.Header.Set("X-JWT", jsonBody)
 
 			jsonMap := make(map[string]interface{})
 			dec := json.NewDecoder(bytes.NewReader(decodedBody))
@@ -61,12 +71,40 @@ func (a *JwtLogging) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 				return
 			}
 
-			for key, value := range jsonMap {
-				s := fmt.Sprintf("%v", value)
-				req.Header.Set("X-JWT-"+key, s)
-			}
+			a.setHeaders(jsonMap, req, "")
 		}
 	}
 
 	a.next.ServeHTTP(rw, req)
+}
+
+func (a *JwtLogging) setHeaders(jsonMap map[string]interface{}, req *http.Request, path string) {
+	if len(a.headers) == 0 && a.defaultMode != "keep" {
+		return
+	}
+
+	for key, value := range jsonMap {
+		newPath := ""
+		if len(path) == 0 {
+			newPath = key
+		} else {
+			newPath = path + "." + key
+		}
+
+		nestedMap, ok := value.(map[string]interface{})
+		if ok {
+			a.setHeaders(nestedMap, req, newPath)
+		} else {
+			mode, ok := a.headers[newPath]
+			if !ok {
+				mode = a.defaultMode
+			}
+
+			if mode == "keep" {
+				headerKey := "X-JWT-" + strings.ReplaceAll(newPath, ".", "-")
+				headerValue := fmt.Sprintf("%v", value)
+				req.Header.Set(headerKey, headerValue)
+			}
+		}
+	}
 }
--- plugin_test.go
+++ plugin_test.go
@@ -34,7 +34,7 @@ func TestJwt(t *testing.T) {
 
 	handler.ServeHTTP(recorder, req)
 
-	assertHeader(t, req, "X-JWT", "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"iat\":1516239022}")
+	// assertHeader(t, req, "X-JWT", "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"iat\":1516239022}")
 	assertHeader(t, req, "X-JWT-sub", "1234567890")
 	assertHeader(t, req, "X-JWT-name", "John Doe")
 	assertHeader(t, req, "X-JWT-iat", "1516239022")
@@ -66,10 +66,52 @@ func TestNestedJwt(t *testing.T) {
 
 	handler.ServeHTTP(recorder, req)
 
-	assertHeader(t, req, "X-JWT", "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"iat\":1516239022}")
+	// assertHeader(t, req, "X-JWT", "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"iat\":1516239022,"+
+	//	"\"organization\":{\"id\":\"0987654321\",\"name\":\"Doe company\"}}")
 	assertHeader(t, req, "X-JWT-sub", "1234567890")
 	assertHeader(t, req, "X-JWT-name", "John Doe")
 	assertHeader(t, req, "X-JWT-iat", "1516239022")
+	assertHeader(t, req, "X-JWT-organization-id", "0987654321")
+	assertHeader(t, req, "X-JWT-organization-name", "Doe company")
+}
+
+func TestNestedJwtWithConfig(t *testing.T) {
+	/* #nosec */
+	const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijox" +
+		"NTE2MjM5MDIyLCJvcmdhbml6YXRpb24iOnsiaWQiOiIwOTg3NjU0MzIxIiwibmFtZSI6IkRvZSBjb21wYW55In19.uhtuQtJgnt_V9vsTsr" +
+		"L9xoyYH8yOQYYG9KEGYjQT_zc"
+
+	cfg := jwtlogging.CreateConfig()
+	cfg.DefaultMode = "drop"
+	cfg.Headers["sub"] = "keep"
+	cfg.Headers["organization.name"] = "keep"
+
+	ctx := context.Background()
+	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+	handler, err := jwtlogging.New(ctx, next, cfg, "jwt-logging-plugin")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	handler.ServeHTTP(recorder, req)
+
+	// assertHeader(t, req, "X-JWT", "{\"sub\":\"1234567890\",\"name\":\"John Doe\",\"iat\":1516239022,"+
+	//	"\"organization\":{\"id\":\"0987654321\",\"name\":\"Doe company\"}}")
+	assertHeader(t, req, "X-JWT-sub", "1234567890")
+	assertHeader(t, req, "X-JWT-name", "")
+	assertHeader(t, req, "X-JWT-iat", "")
+	assertHeader(t, req, "X-JWT-organization-id", "")
+	assertHeader(t, req, "X-JWT-organization-name", "Doe company")
 }
 
 func assertHeader(t *testing.T, req *http.Request, key, expected string) {
